1bc1c90de414254743aba199ccc1d4664f35f079
JojoDuke <jojoamankwa@gmail.com>
2021-08-21 02:00:16 +0000

    got server running

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express()
+
+app.set("view-engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'Duke' });
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+})
+
+app.listen(3000);
